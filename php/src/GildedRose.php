c2a6ef5d58c8c33fab4c48de00ee55433e5892f2
tptadpole <wk7293@gmail.com>
2022-04-06 17:19:25 +0800

    用 Extract Function 來處理重複的程式碼

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -24,44 +24,46 @@ final class GildedRose
             }
 
             if ($item->name == 'Aged Brie') {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+                $this->increaseQuality($item);
             } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+                $this->increaseQuality($item);
                 if ($item->sell_in < 11) {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $this->increaseQuality($item);
                 }
                 if ($item->sell_in < 6) {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $this->increaseQuality($item);
                 }
             } else {
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;
-                }
+                $this->decreaseQuality($item);
             }
 
             $item->sell_in = $item->sell_in - 1;
 
             if ($item->sell_in < 0) {
                 if ($item->name == 'Aged Brie') {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $this->increaseQuality($item);
                 } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                     $item->quality = $item->quality - $item->quality;
                 } else {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
+                    $this->decreaseQuality($item);
                 }
             }
         }
+    }
+
+    private function increaseQuality(Item $item)
+    {
+        if ($item->quality >= 50) {
+            return;
+        }
+        $item->quality += 1;
+    }
+
+    private function decreaseQuality(Item $item)
+    {
+        if ($item->quality <= 0) {
+            return;
+        }
+        $item->quality -= 1;
     }
 }
